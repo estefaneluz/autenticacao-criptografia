8286347a64aeb2acb4c17a010d96e372313c6fe5
estefaneluz <estefanecortes@gmail.com>
2021-08-06 17:27:06 -0300

    cadastroUsuario ok com senha criptografada

--- classe-01/src/controllers/usuarios.js
+++ classe-01/src/controllers/usuarios.js
@@ -1,4 +1,6 @@
 const conexao = require('../connection');
+const securePassword = require('secure-password');
+const pwd = securePassword();
 
 const cadastrarUsuario = async (req, res) => {
     const { nome, email, senha } = req.body;
@@ -8,11 +10,16 @@ const cadastrarUsuario = async (req, res) => {
 
     try {
         const emailUnico = await conexao.query("SELECT * FROM usuarios WHERE email = $1", [email]);
-        if(emailUnico){
+        if(emailUnico.rowCount > 0){
             return res.status(400).json("Esse e-mail já existe.")
         }
 
+        const hash = (await pwd.hash(Buffer.from(senha))).toString("hex");
+        const usuario = await conexao.query("INSERT INTO usuarios(nome, email, senha) VALUES($1, $2, $3)",[nome, email, hash]);
 
+        if(!usuario.rowCount) return res.status(400).json("Não foi possível cadastrar o usuário");
+
+        return res.status(200).json("Usuário cadastrado com sucesso.");
     } catch(error){
         return res.status(error.status).json(error.message);
     }
