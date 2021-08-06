e33f72c833d8c20bf2f989ea99d5d31ad7ab2e60
estefaneluz <estefanecortes@gmail.com>
2021-08-06 16:40:06 -0300

    arquivo rotas criado

---
+++ classe-01/src/routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const usuarios = require('./controllers/usuarios');
+const pokemons = require('./controllers/pokemons');
+
+const routes = express();
+
+module.exports = routes;
