d1d6422bd8cce82fae280011fd29235b50372b4b
estefaneluz <estefanecortes@gmail.com>
2021-08-06 17:13:03 -0300

    verificacao do body em cadastrarUsuario

--- classe-01/src/controllers/usuarios.js
+++ classe-01/src/controllers/usuarios.js
@@ -1,7 +1,21 @@
 const conexao = require('../connection');
 
 const cadastrarUsuario = async (req, res) => {
-    
+    const { nome, email, senha } = req.body;
+    if(!nome) return res.status(400).json("É necessário informar o nome.");
+    if(!email) return res.status(400).json("É necessário informar o e-mail.");
+    if(!senha) return res.status(400).json("É necessário informar a senha.");
+
+    try {
+        const emailUnico = await conexao.query("SELECT * FROM usuarios WHERE email = $1", [email]);
+        if(emailUnico){
+            return res.status(400).json("Esse e-mail já existe.")
+        }
+
+
+    } catch(error){
+        return res.status(error.status).json(error.message);
+    }
 }
 
 module.exports = { cadastrarUsuario };
--- classe-01/src/routes.js
+++ classe-01/src/routes.js
@@ -4,4 +4,7 @@ const pokemons = require('./controllers/pokemons');
 
 const routes = express();
 
+//usuarios
+routes.post('/usuarios', usuarios.cadastrarUsuario);
+
 module.exports = routes;
